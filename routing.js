2b7e5aa117d5b31ca9b70bec2000de225df2e289
flatironschool <t.benson@live.co.uk>
2020-05-27 19:24:08 +0100

    added routing and grabbing data from response

---
+++ routing.js
@@ -0,0 +1,54 @@
+// using server routing in node.js
+
+const http = require('http')
+const fs = require('fs')
+
+// create server - will continue running as long as there are event listeners active
+const server = http.createServer((req, res) => {
+    // create URL variable
+    const url = req.url
+    const method = req.method
+    // create routes
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        // tells server this is the end of the response, will not send back anything after this.
+        return res.end()
+    }
+    if (url === '/message' && method === 'POST') {
+        // create body object
+        const body = []
+        // event listener for when a new data event is fired. Parse data by pushing in chunks into the body object.
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        // when finished parsing data convert body array into single object & turn to string
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            // grab content of message and create file with content of message
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
+
+        // set status code to 302: redirect, 
+        res.statusCode = 302
+        // set header to tell it where to redirect to
+        res.setHeader('Location', '/')
+        return res.end()
+    }
+
+    // sets the response headers - set type of response to text/html 
+    res.setHeader('Content-Type', 'text/html')
+    // sets the content of the response
+    res.write('<html>')
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+    res.write('</html>')
+    // tells server this is the end of the response, will not send back anything after this.
+    res.end()
+})
+
+// create listener for server on specified port
+server.listen(3003)
