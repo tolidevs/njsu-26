828fcca6d275d5c7c4d6a5c2511ac076d8ea92fd
flatironschool <t.benson@live.co.uk>
2020-05-27 18:08:03 +0100

    create server

---
+++ app.js
@@ -0,0 +1,12 @@
+// creating a server in node.js
+
+
+const http = require('http')
+
+// create server 
+const server = http.createServer((req, res) => {
+    
+})
+
+// listen to server on specified port
+server.listen(3003)
