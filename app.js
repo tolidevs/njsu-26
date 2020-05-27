cafa486f0540fab36659d729cd615a5040a040ee
flatironschool <t.benson@live.co.uk>
2020-05-27 18:39:02 +0100

    added how to write a response

--- app.js
+++ app.js
@@ -3,10 +3,20 @@
 
 const http = require('http')
 
-// create server 
+// create server - will continue running as long as there are event listeners active
 const server = http.createServer((req, res) => {
-    
+    // these are the important properties
+    console.log(req.url, req.method, req.headers)
+    // sets the response headers - set type of response to text/html 
+    res.setHeader('Content-Type', 'text/html')
+    // sets the content of the response
+    res.write('<html>')
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+    res.write('</html>')
+    // tells server this is the end of the response, will not send back anything after this.
+    res.end()
 })
 
-// listen to server on specified port
+// create listener for server on specified port
 server.listen(3003)
